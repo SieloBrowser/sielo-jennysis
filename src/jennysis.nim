4213555eeb47dbc314f7ffe84612a4bd6b3023c5
citorva <duet.venceslas@orange.fr>
2019-07-09 14:08:39 +0200

    Remove junk code

--- src/jennysis.nim
+++
@@ -1,7 +0,0 @@
-# This is just an example to get you started. A typical hybrid package
-# uses this file as the main entry point of the application.
-
-import jennysispkg/submodule
-
-when isMainModule:
-  echo(getWelcomeMessage())
